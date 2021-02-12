20b06ff13f03797ffefedd582358ca03b402cb91
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-12 17:33:59 +0000

    fixed trailing spaces i think

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.getElementById('button').addEventListener('click', addition)
 
 function addition () {
   number = document.getElementById('input').value
-  
+
   number = parseInt(number)
 
   number = number * 7
-  
+
   alert(number)
 }
