18cd9efc6a0d3776935343a7a684f90fe22c145a
SophiaSamera <Sophia.samera@ocsbstudent.ca>
2021-02-15 23:23:28 +0000

    added let

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.getElementById('button').addEventListener('click', calculateArea)
+let number = 0
+let width = number
+let height = number
 
 function calculateArea () {
-  width = document.getElementById("width").value
-  height = document.getElementById("height").value
+  width = document.getElementById('width').value
+  height = document.getElementById('height').value
   area = width * height
   document.getElementById('area').value = area
 }
